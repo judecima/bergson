2729bb1fac7b80065ba6df90def06e752a5582c1
Julio Decima <juliodecima@gmail.com>
2020-03-16 21:52:13 -0300

    correccion siniestros

--- src/routes/index.js
+++ src/routes/index.js
@@ -97,13 +97,14 @@ router.post('/new-contact', async(req, res) => {
 
 
 router.post('/new-siniestro', async(req, res) => {
-    if (req.method === 'OPTIONS') {
-        // Send response to OPTIONS requests
-        res.set('Access-Control-Allow-Methods', 'GET');
-        res.set('Access-Control-Allow-Headers', 'Content-Type');
-        res.set('Access-Control-Max-Age', '3600');
-        res.status(204).send('');
-      } else {
+    res.set('Access-Control-Allow-Origin', '*');
+        if (req.method === 'OPTIONS') {
+            // Send response to OPTIONS requests
+            res.set('Access-Control-Allow-Methods', 'GET');
+            res.set('Access-Control-Allow-Headers', 'Content-Type');
+            res.set('Access-Control-Max-Age', '3600');
+            res.status(204).send('');
+          } else {
     
     const newSiniestro = {
         
