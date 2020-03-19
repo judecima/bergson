9eddcc7957a04a009419c6a9318aca928c8cf41d
Julio Decima <juliodecima@gmail.com>
2020-03-19 10:10:38 -0300

    puerto2

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 // Settings
 app.set('port', process.env.PORT || 3000);
-// var port = process.env.PORT || 3000;
+
 
 //     var server = app.listen(port, function () {
 //         console.log('Server running at http://127.0.0.1:' + port + '/');
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-
+let port = process.env.PORT || 3000;
 async function main() {
     // await app.listen(3000);
     // console.log('Server on port', 3000);
