d8509c4bed17b7a175ce71188946017e0b068417
Julio Decima <juliodecima@gmail.com>
2020-03-24 06:34:32 -0300

    mod aws

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const app = require('./app');
-// let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 async function main() {
-    await app.listen(3000);
-    console.log('Server on port', 3000);
+    await app.listen(port);
+    console.log('Server on port', port);
     
 
 }
